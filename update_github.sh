a018082cf200bad8f2a2061a3490db8930afc36f
arensburger <arensburger@gmail.com>
2025-04-26 15:45:11 -0700

    auto commit

--- update_github.sh
+++ update_github.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 file1="./mainscript.pl"
-file2="./lib/*"
-file3="README.md"
+file2="./lib/TEdiscovery.pm"
+file3="./lib/help.txt"
+file4="README.md"
+file5="update_github.sh"
 
 git commit -m 'auto commit' $file1
 git commit -m 'auto commit' $file2
 git commit -m 'auto commit' $file3
+git commit -m 'auto commit' $file4
+git commit -m 'auto commit' $file5
 
 git push origin main
