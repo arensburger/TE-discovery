98298a6df6dca1256858b3e2a64691b9cd06a0aa
arensburger <arensburger@gmail.com>
2025-05-09 19:49:09 -0700

    auto commit

--- helper_scripts/update_github.sh
+++ helper_scripts/update_github.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-file1="./mainscript.pl"
-file2="./TEdiscovery.pm"
-file3="./randomfasta.pl"
-file4="README.md"
+file1="../mainscript.pl"
+file4="../README.md"
 file5="update_github.sh"
-file6="./images/*"
+file6="../images/*"
 
 git commit -m 'auto commit' $file1
-git commit -m 'auto commit' $file2
-git commit -m 'auto commit' $file3
 git commit -m 'auto commit' $file4
 git commit -m 'auto commit' $file5
 git commit -m 'auto commit' $file6
